a2a727831ea6eb614b44086a8b5a579835430e63
Kipras <reg@anaweb.lt>
2020-10-08 21:46:45 +0300

    Template for an invoice

---
+++ resources/views/pdf/invoice.blade.php
@@ -0,0 +1,81 @@
+<html>
+<head>
+    <link rel="stylesheet" href="{{ asset('css/pdf.css') }}">
+</head>
+<body>
+<div>
+    <h1 class="text-align-center">
+        <h4 class="super-small-margin">Sąskaita faktūra</h4>
+        <p class="no-margin">Serija ir Nr. SF 123</p>
+        <p class="no-margin">Sąskaitos data 2020-08-11</p>
+    </h1>
+</div>
+
+<table class="full-width">
+    <tr>
+        <td class="half-width">
+            <div><strong>Pardavėjas</strong></div>
+            <div>Kipras Bielinskas</div>
+            <div>Individualios veiklos pažymos nr. 761707</div>
+            <div>Asmens kodas: 39808240516</div>
+            <div>Nemuno krantinė 26-6, Kaunas, LT-45273, Lietuva</div>
+            <div>+37064141272</div>
+            <div>info@anaweb.lt</div>
+            <div>www.anaweb.lt</div>
+            <div>Swedbank — LT117300010139945347</div>
+        </td>
+        <td class="half-width vertical-align-top">
+            <div><strong>Pirkėjas</strong></div>
+            <div>Promas LT, VšĮ</div>
+            <div>Įm. kodas 304690879</div>
+            <div>PVM mokėtojo kodas LT100011469319</div>
+            <div>Beržų g. 10, Gulbiniškių k., Jonavos r. , Lietuva</div>
+        </td>
+    </tr>
+</table>
+
+<div class="margin-top-normal">
+    <table style="border-spacing: unset">
+        <thead>
+        <tr>
+            <th class="border-bottom">Pavadinimas</th>
+            <th class="border-bottom">Kiekis</th>
+            <th class="border-bottom">Matas</th>
+            <th class="border-bottom">Kaina</th>
+            <th class="border-bottom">Išviso</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td class="border-bottom">Sistemos darbai. Galimybė keisti jau sukurtos konsultacijos temą.</td>
+            <td class="border-bottom text-align-center">1</td>
+            <td class="border-bottom text-align-center">vnt</td>
+            <td class="border-bottom text-align-center">84.00 &euro;</td>
+            <td class="border-bottom text-align-center">84.00 &euro;</td>
+        </tr>
+        <tr>
+            <td class="border-bottom">Sistemos darbai. Duomenų bazės struktūros keitimas. Galimybė įvesti daugiau nei
+                vieną pertrauką konsultacijai. Kiti smulkūs logikos patobūlinimai.
+            </td>
+            <td class="border-bottom text-align-center">1</td>
+            <td class="border-bottom text-align-center">vnt</td>
+            <td class="border-bottom text-align-center">145.00 &euro;</td>
+            <td class="border-bottom text-align-center">145.00 &euro;</td>
+        </tr>
+        </tbody>
+    </table>
+</div>
+
+<div class="text-align-right">
+    <div><strong>Bendra suma</strong> 229.00 &euro;</div>
+</div>
+
+<div class="margin-top-normal">
+    Suma žodžiais : Du šimtai dvidešimt devyni Eur ir 0 ct
+</div>
+<div class="margin-top-normal">
+    Sąskaitą išrašė: Kipras Bielinskas
+</div>
+
+</body>
+</html>
