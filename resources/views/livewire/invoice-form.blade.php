fb42b9cfdcd25725b256c82e71e9339eb3ba3dbe
Kipras <reg@anaweb.lt>
2020-10-10 21:24:46 +0300

    BUG FIX. Changed submit button to submit input. Solves JS front-end problem.

--- resources/views/livewire/invoice-form.blade.php
+++ resources/views/livewire/invoice-form.blade.php
@@ -2,43 +2,43 @@
     <form class="rounded w-full px-20" wire:submit.prevent="submit">
         <div class="flex flex-wrap -mx-3 mb-5 mt-4">
             <div class="w-full px-3">
-                <label class="block uppercase tracking-wide text-xs font-bold mb-2" for="company-name">
+                <label class="block uppercase tracking-wide text-xs font-bold mb-2" for="company_name">
                     Įmonės pavadinimas
                 </label>
                 <input wire:model="companyName"
                        class="appearance-none block w-full border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                       id="company-name" type="text" placeholder="Testas, UAB">
+                       id="company_name" type="text" placeholder="Testas, UAB">
                 @error('companyName') <span class="text-red-600">{{ $message }}</span> @enderror
             </div>
         </div>
         <div class="flex flex-wrap -mx-3 mb-5">
             <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                <label class="block uppercase tracking-wide text-xs font-bold mb-2" for="company-code">
+                <label class="block uppercase tracking-wide text-xs font-bold mb-2" for="company_code">
                     Įmonės kodas
                 </label>
                 <input wire:model="companyCode"
                        class="appearance-none block w-full border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                       id="company-code" type="text" placeholder="123456789">
+                       id="company_code" type="text" placeholder="123456789">
                 @error('companyCode') <span class="text-red-600">{{ $message }}</span> @enderror
             </div>
             <div class="w-full md:w-1/2 px-3">
-                <label class="block uppercase tracking-wide text-xs font-bold mb-2" for="company-vat">
+                <label class="block uppercase tracking-wide text-xs font-bold mb-2" for="company_vat">
                     PVM kodas
                 </label>
                 <input wire:model="companyVat"
                        class="appearance-none block w-full border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                       id="company-vat" type="text" placeholder="123456789">
+                       id="company_vat" type="text" placeholder="123456789">
                 @error('companyVat') <span class="text-red-600">{{ $message }}</span> @enderror
             </div>
         </div>
         <div class="flex flex-wrap -mx-3">
             <div class="w-full px-3">
-                <label class="block uppercase tracking-wide text-xs font-bold mb-2" for="company-address">
+                <label class="block uppercase tracking-wide text-xs font-bold mb-2" for="company_address">
                     Įmonės adresas
                 </label>
                 <input wire:model="companyAddress"
                        class="appearance-none block w-full border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                       id="company-address" type="text" placeholder="Vileikos g. 8, Kaunas">
+                       id="company_address" type="text" placeholder="Vileikos g. 8, Kaunas">
                 @error('companyAddress') <span class="text-red-600">{{ $message }}</span> @enderror
             </div>
         </div>
@@ -50,40 +50,40 @@
         @foreach($productList as $index => $product)
             <div class="flex flex-wrap -mx-3 mb-5">
                 <div class="w-full md:w-1/4 px-3">
-                    <label class="block uppercase tracking-wide text-xs font-bold mb-2" for="productList.{{ $index }}.product-name">
+                    <label class="block uppercase tracking-wide text-xs font-bold mb-2" for="productList.{{ $index }}.product_name">
                         Paslaugos pavadinimas
                     </label>
-                    <input wire:model="productList.{{ $index }}.product-name" name="productList[{{ $index }}][product-name]"
+                    <input wire:model="productList.{{ $index }}.product_name" name="productList[{{ $index }}][product_name]"
                            class="appearance-none block w-full border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                           id="product-name{{ $index }}" type="text" placeholder="Produktas">
-                    @error('productList.' . $index . '.product-name') <span class="text-red-600">{{ $message }}</span> @enderror
+                           id="product_name{{ $index }}" type="text" placeholder="Produktas">
+                    @error('productList.' . $index . '.product_name') <span class="text-red-600">{{ $message }}</span> @enderror
                 </div>
                 <div class="flex-1 px-3">
-                    <label class="block uppercase tracking-wide text-xs font-bold mb-2" for="product-price{{ $index }}">
+                    <label class="block uppercase tracking-wide text-xs font-bold mb-2" for="product_price{{ $index }}">
                         Kaina
                     </label>
-                    <input  wire:model="productList.{{ $index }}.product-price" name="productList[{{ $index }}][product-price]"
+                    <input  wire:model="productList.{{ $index }}.product_price" name="productList[{{ $index }}][product_price]"
                            class="appearance-none block w-full border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                           id="product-price{{ $index }}" type="text" placeholder="125">
-                    @error('productList.' . $index . '.product-price') <span class="text-red-600">{{ $message }}</span> @enderror
+                           id="product_price{{ $index }}" type="text" placeholder="125">
+                    @error('productList.' . $index . '.product_price') <span class="text-red-600">{{ $message }}</span> @enderror
                 </div>
                 <div class="flex-1 px-3">
-                    <label class="block uppercase tracking-wide text-xs font-bold mb-2" for="product-pcs{{ $index }}">
+                    <label class="block uppercase tracking-wide text-xs font-bold mb-2" for="product_pcs{{ $index }}">
                         Kiekis
                     </label>
-                    <input wire:model="productList.{{ $index }}.product-pcs" name="productList[{{ $index }}][product-pcs]"
+                    <input wire:model="productList.{{ $index }}.product_pcs" name="productList[{{ $index }}][product_pcs]"
                            class="appearance-none block w-full border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                           id="product-pcs{{ $index }}" type="text" placeholder="10">
-                    @error('productList.' . $index . '.product-pcs') <span class="text-red-600">{{ $message }}</span> @enderror
+                           id="product_pcs{{ $index }}" type="text" placeholder="10">
+                    @error('productList.' . $index . '.product_pcs') <span class="text-red-600">{{ $message }}</span> @enderror
                 </div>
                 <div class="flex-1 px-3">
-                    <label class="block uppercase tracking-wide text-xs font-bold mb-2" for="productList[{{ $index }}][pcs-type]">
+                    <label class="block uppercase tracking-wide text-xs font-bold mb-2" for="productList[{{ $index }}][pcs_type]">
                         Vienetai
                     </label>
-                    <input wire:model="productList.{{ $index }}.pcs-type"
+                    <input wire:model="productList.{{ $index }}.pcs_type"
                            class="appearance-none block w-full border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                           name="productList[{{ $index }}][pcs-type]" type="text">
-                    @error('productList.' . $index . '.pcs-type') <span class="text-red-600">{{ $message }}</span> @enderror
+                           name="productList[{{ $index }}][pcs_type]" type="text">
+                    @error('productList.' . $index . '.pcs_type') <span class="text-red-600">{{ $message }}</span> @enderror
                 </div>
                 <div class="flex-1 px-3">
                     <button wire:click.prevent="deleteProduct({{ $index }})">Pašalinti</button>
@@ -96,9 +96,7 @@
         </div>
 
         <div class="float-right mb-4">
-            <button class="bg-green-400 hover:bg-green-500 text-white font-bold py-2 px-4 rounded" type="submit">
-                Generuoti PDF
-            </button>
+            <input class="bg-green-400 hover:bg-green-500 text-white font-bold py-2 px-4 rounded" type="submit" value="Generuoti PDF">
         </div>
     </form>
 </div>
