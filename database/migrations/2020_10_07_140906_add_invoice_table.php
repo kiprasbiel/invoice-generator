b9a7daec9300d2529fa8f70f7c6295a991e4b342
Kipras <reg@anaweb.lt>
2020-10-13 16:46:57 +0300

    Migration for Invoice table

---
+++ database/migrations/2020_10_07_140906_add_invoice_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddInvoiceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->string('company_name');
+            $table->string('company_code');
+            $table->string('company_vat')->nullable();
+            $table->string('company_address')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoices');
+    }
+}
