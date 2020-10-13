31780643bbc7c9ca5f8974c682f1bc71c78fefd9
Kipras <reg@anaweb.lt>
2020-10-13 16:44:51 +0300

    Adding User one to many relation with Invoices

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,8 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    public function invoices(){
+        return $this->hasMany('App\Models\Invoice');
+    }
 }
