308c7b8f01947a9fc9e5a853c6853ffe791b2067
Kipras <kipras@anaweb.lt>
2020-11-16 23:21:05 +0200

    Updating dependency

--- public/js/app.js
+++ public/js/app.js
@@ -19384,8 +19384,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/kiprasbiel/kursinis/apskaita/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/kiprasbiel/kursinis/apskaita/resources/css/app.css */"./resources/css/app.css");
+__webpack_require__(/*! /var/www/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/resources/css/app.css */"./resources/css/app.css");
 
 
 /***/ })
