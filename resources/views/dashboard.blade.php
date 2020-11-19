654398484b40da0a83d27ac609ac1adbc9971b6a
Kipras <kipras@anaweb.lt>
2020-11-19 16:27:45 +0200

    Editing default dashboard so that it opens HomeDash livewire component instead of Welcome component

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,16 +1,8 @@
 <x-app-layout>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded float-right">
-                Kurti sąskaitą
-            </button>
-        </div>
-
-    </div>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+                @livewire('homedash')
             </div>
         </div>
     </div>
