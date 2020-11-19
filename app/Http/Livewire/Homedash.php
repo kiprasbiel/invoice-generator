b75a66b83cc3b3400e3cd59e4e9d04058b594530
Kipras <kipras@anaweb.lt>
2020-11-19 16:28:38 +0200

    Showing total income, total expenses and total tax inside Users dashboard

--- app/Http/Livewire/Homedash.php
+++ app/Http/Livewire/Homedash.php
@@ -6,10 +6,28 @@ use Livewire\Component;
 
 class Homedash extends Component
 {
-    public $testVar = "Home dash component";
+    public $income;
+    public $expenses;
+    public $totalTax;
+
+    public $gpm;
+    public $psd;
+    public $vsd;
 
     public function render()
     {
         return view('livewire.homedash');
+    }
+
+    public function mount(){
+        $user = auth()->user();
+
+        $this->income = $user->getTotalIncome();
+        $this->expenses = $user->getTotalExpenses();
+        $this->totalTax = $user->getTotalTax();
+
+        $this->gpm = $user->getGPM();
+        $this->psd = $user->getPSD();
+        $this->vsd = $user->getVSD();
     }
 }
--- resources/views/livewire/homedash.blade.php
+++ resources/views/livewire/homedash.blade.php
@@ -1,3 +1,30 @@
-<x-app-layout>
-    <h1>Analytics goes here</h1>
-</x-app-layout>
+<div class="flex flex-wrap overflow-hidden text-center py-8 px-4">
+
+    <div class="w-full overflow-hidden sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/3 ">
+        <div class="w-full">
+            Išrašytų sąskaitų suma
+        </div>
+        <div class="w-full text-4xl">
+            {{ $income }} &euro;
+        </div>
+    </div>
+
+    <div class="w-full overflow-hidden sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/3">
+        <div class="w-full">
+            Sąnaudų suma
+        </div>
+        <div class="w-full text-4xl">
+            {{ $expenses }} &euro;
+        </div>
+    </div>
+
+    <div class="w-full overflow-hidden sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/3">
+        <div class="w-full">
+            Mokėtini mokesčiai
+        </div>
+        <div class="w-full text-4xl">
+            {{ $totalTax }} &euro;
+        </div>
+    </div>
+
+</div>
