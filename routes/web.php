12bc0c190e3518ad4cac0664a0cf8b0bc598c9cd
Kipras <kipras@anaweb.lt>
2020-11-19 16:25:15 +0200

    Changing changing dashboard route to default jetstream

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/', function () {
-    return view('livewire.homedash');
+    return view('dashboard');
 })->name('dashboard');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/invoice/create', function () {
