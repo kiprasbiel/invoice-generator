a8537649c59f46ef861aa4f73a309905e177e9b9
Kipras <kipras@anaweb.lt>
2020-11-23 17:03:00 +0200

    GMP now calculates acording to privileges

--- app/Models/Taxes/GPM.php
+++ app/Models/Taxes/GPM.php
@@ -7,16 +7,25 @@ use App\Models\Tax;
 
 class GPM extends Tax
 {
-    protected $rate;
+    private $rate;
 
-    function __construct($income, $expenses)
-    {
+    function __construct($income, $expenses) {
         parent::__construct($income, $expenses);
-        $this->rate = 0.05; // TODO: perkelti i DB. Gali buti 15% arba 5%
     }
 
-    public function getCalcGPM()
-    {
+    private function setGPMRate() {
+        $user = auth()->user();
+        $isFreeMarket = $user->getPrivilege('isFreeMarketActivity');
+        if($isFreeMarket === 'yes') {
+            $this->rate = 0.15; // TODO: perkelti i DB. Gali buti 15% arba 5%
+        } else {
+            $this->rate = 0.05;
+        }
+
+    }
+
+    public function getCalcGPM() {
+        $this->setGPMRate();
         return round($this->getPayableWage() * $this->rate, 2);
     }
 }
