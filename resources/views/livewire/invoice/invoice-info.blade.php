f7d6267af1e33eb6a21d3c7f84cdec167d7d415c
Kipras <kipras@anaweb.lt>
2020-11-26 16:10:25 +0200

    Adding invoice list item's info table for invoice product displayment

--- resources/views/livewire/invoice/invoice-info.blade.php
+++ resources/views/livewire/invoice/invoice-info.blade.php
@@ -1,12 +1,59 @@
-<tr class="{{ $display }}">
-    <td>
-        <h5>Produktai</h5>
-        @if(isset($items))
-            @foreach($items as $item)
-                <div class="full-width">
-                    {{ $item->name }} {{ $item->price }}
-                </div>
-            @endforeach
-        @endif
+<tr class="{{ $display }} border-none">
+    <td colspan="6" class="px-4">
+        <div>
+            <div class="bg-gray-200 p-2 rounded-lg">
+                <h5>Produktai</h5>
+            </div>
+            <table class="min-w-full">
+                <thead>
+                <tr>
+                    <th scope="col"
+                        class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Pavadinimas
+                    </th>
+                    <th scope="col"
+                        class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Vienetai
+                    </th>
+                    <th scope="col"
+                        class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Kiekis
+                    </th>
+                    <th scope="col"
+                        class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Kaina
+                    </th>
+                    <th scope="col"
+                        class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Viso kaina
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+                @if(isset($items))
+                    @foreach($items as $item)
+                        <tr>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                <div class="text-sm text-gray-900">{{$item->name}}</div>
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                <div class="text-sm text-gray-900">{{$item->unit}}</div>
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                <div class="text-sm text-gray-900">{{$item->quantity}}</div>
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                <div class="text-sm text-gray-900">{{$item->price}} &euro;</div>
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                <div class="text-sm text-gray-900">{{$item->getTotalPrice()}} &euro;</div>
+                            </td>
+                        </tr>
+                    @endforeach
+                @endif
+                </tbody>
+            </table>
+        </div>
+
     </td>
 </tr>
