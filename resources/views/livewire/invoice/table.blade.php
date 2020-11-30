be6c6e6fc92000f733d5246c508ea8b87c721b91
Kipras <kipras@anaweb.lt>
2020-11-30 17:03:56 +0200

    Added a message when there are no invoices to show. Removed reduntant bottom border.

--- resources/views/livewire/invoice/table.blade.php
+++ resources/views/livewire/invoice/table.blade.php
@@ -1,7 +1,7 @@
 <div class="flex flex-col">
     <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-            <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <div class="shadow overflow-hidden border-gray-200 sm:rounded-lg">
                 <table class="min-w-full divide-y divide-gray-200">
                     <thead>
                     <tr>
@@ -38,8 +38,14 @@
                     @endforeach
                     </tbody>
                 </table>
+
+
+
             </div>
         </div>
     </div>
+    @if(count($invoices) === 0)
+        <div class="text-center py-3">Neturite sukūrę sąskaitų-faktūrų</div>
+    @endif
 </div>
 
