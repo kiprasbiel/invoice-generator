191673a174d844842675a8ca454a160cb47664d9
Kipras <kipras@anaweb.lt>
2020-12-17 19:10:58 +0200

    Translating register view to LT

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,32 +10,32 @@
             @csrf
 
             <div>
-                <x-jet-label value="{{ __('Name') }}" />
+                <x-jet-label value="{{ __('Vardas') }}" />
                 <x-jet-input class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             </div>
 
             <div class="mt-4">
-                <x-jet-label value="{{ __('Email') }}" />
+                <x-jet-label value="{{ __('El. pašto adresas') }}" />
                 <x-jet-input class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
             </div>
 
             <div class="mt-4">
-                <x-jet-label value="{{ __('Password') }}" />
+                <x-jet-label value="{{ __('Slaptažodis') }}" />
                 <x-jet-input class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
             </div>
 
             <div class="mt-4">
-                <x-jet-label value="{{ __('Confirm Password') }}" />
+                <x-jet-label value="{{ __('Pakartokite slaptažodį') }}" />
                 <x-jet-input class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
+                    {{ __('Jau turite paskyrą?') }}
                 </a>
 
                 <x-jet-button class="ml-4">
-                    {{ __('Register') }}
+                    {{ __('Registruotis') }}
                 </x-jet-button>
             </div>
         </form>
