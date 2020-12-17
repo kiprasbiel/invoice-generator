5927cb7f8adabb3d520889fb859cf5e36ff6642b
Kipras <kipras@anaweb.lt>
2020-12-17 19:07:26 +0200

    Removing Forgot your password field from login page and translating it to LT

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,31 +16,31 @@
             @csrf
 
             <div>
-                <x-jet-label value="{{ __('Email') }}" />
+                <x-jet-label value="{{ __('El. pašto adresas') }}" />
                 <x-jet-input class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
             </div>
 
             <div class="mt-4">
-                <x-jet-label value="{{ __('Password') }}" />
+                <x-jet-label value="{{ __('Slaptažodis') }}" />
                 <x-jet-input class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
             </div>
 
             <div class="block mt-4">
                 <label class="flex items-center">
                     <input type="checkbox" class="form-checkbox" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    <span class="ml-2 text-sm text-gray-600">{{ __('Prisiminti mane') }}</span>
                 </label>
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
+{{--                @if (Route::has('password.request'))--}}
+{{--                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">--}}
+{{--                        {{ __('Forgot your password?') }}--}}
+{{--                    </a>--}}
+{{--                @endif--}}
 
                 <x-jet-button class="ml-4">
-                    {{ __('Login') }}
+                    {{ __('Prisijungti') }}
                 </x-jet-button>
             </div>
         </form>
