a6378fb08a7f49f26685d94e49ccedd33b013724
Kipras <kipras@anaweb.lt>
2020-12-17 19:16:16 +0200

    Translating guest header to LT

--- resources/views/livewire/guest-header.blade.php
+++ resources/views/livewire/guest-header.blade.php
@@ -1,12 +1,12 @@
 <nav class="bg-gray-100 sm:flex-row text-right sm:justify-between py-4 px-6 sm:items-baseline w-full">
     <div>
         @if(\Request::is('register'))
-            <a href="{{ route('login') }}" class="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">Login</a>
+            <a href="{{ route('login') }}" class="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">Prisijungti</a>
         @elseif(\Request::is('login'))
-            <a href="{{ route('register') }}" class="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">Register</a>
+            <a href="{{ route('register') }}" class="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">Registruotis</a>
         @else
-            <a href="{{ route('login') }}" class="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">Login</a>
-            <a href="{{ route('register') }}" class="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">Register</a>
+            <a href="{{ route('login') }}" class="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">Prisijungti</a>
+            <a href="{{ route('register') }}" class="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">Registruotis</a>
         @endif
     </div>
 </nav>
