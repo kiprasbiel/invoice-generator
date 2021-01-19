cd43a55748e0aaeb4eaac151df3f398eb07400c8
Kipras <kipras@anaweb.lt>
2021-01-19 20:47:35 +0200

    Tax Calc test with expenses over 30proc

--- tests/Feature/TaxCalcTest.php
+++ tests/Feature/TaxCalcTest.php
@@ -10,6 +10,12 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
+
+/*
+ * Duomenys tikrinti Sodra skaiciuokleje:
+ * 2021-01-19
+ */
+
 class TaxCalcTest extends TestCase
 {
     protected $user;
@@ -23,16 +29,25 @@ class TaxCalcTest extends TestCase
     public function testGPMCalc() {
         $gpm = new GPM(10000, 100);
         $this->assertEquals(350, $gpm->getCalcGPM());
+
+        $gpm = new GPM(10000, 4000);
+        $this->assertEquals(240.77, $gpm->getCalcGPM());
     }
 
     public function testPSDCalc() {
         $psd = new PSD(10000, 100);
+        $this->assertEquals(508.44, $psd->getCalcPSD());
+
+        $psd = new PSD(10000, 4000);
         $this->assertEquals(508.44, $psd->getCalcPSD());
     }
 
     public function testVSDCalc() {
         $vsd = new VSD(10000, 100);
         $this->assertEquals(788.76, $vsd->getCalcVSD());
+
+        $vsd = new VSD(10000, 4000);
+        $this->assertEquals(676.08, $vsd->getCalcVSD());
     }
 
     protected function tearDown(): void {
