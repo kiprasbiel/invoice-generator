3be12bdfdf0597941dafcc44892d73542365630b
Kipras <kipras@anaweb.lt>
2021-01-19 17:03:17 +0200

    Created UITest for asserting UI elements

---
+++ tests/Feature/UITest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UITest extends TestCase
+{
+
+}
