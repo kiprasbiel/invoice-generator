01cb5ff290180f2b5c4fbd5bfa7adf8fd60170d1
Kipras <kipras@anaweb.lt>
2021-02-03 16:12:50 +0200

    While creating a User factory, also related and required Meta information for the User is created

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Meta;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
@@ -29,5 +30,32 @@ class UserFactory extends Factory
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
+    }
+
+    public function configure()
+    {
+        // Adding user Settings
+        return $this->afterCreating(function (User $user) {
+            Meta::factory()->create([
+                'name' => 'userActivitySettings',
+                'value' => '{"full_name":"' . $user->name .  '","iv_code":null,"personal_code":null,"address":null,"phone":null,"email":"' . $user->email . '","additional_info":null,"bank_name":null,"bank_account_num":null,"vat":null}',
+                'metable_id' => $user->id,
+            ]);
+            Meta::factory()->create([
+                'name' => 'sfNumberSettings',
+                'value' => '{"sf_code":"SF"}',
+                'metable_id' => $user->id,
+            ]);
+            Meta::factory()->create([
+                'name' => 'privilegesSettings',
+                'value' => '{"isStudent":null,"isFirstTimer":null,"isPensioner":null,"additionalPension":"pens0","isFreeMarketActivity":null}',
+                'metable_id' => $user->id,
+            ]);
+            Meta::factory()->create([
+                'name' => 'totalExpenses',
+                'value' => '{"expenses":"0"}',
+                'metable_id' => $user->id,
+            ]);
+        });
     }
 }
