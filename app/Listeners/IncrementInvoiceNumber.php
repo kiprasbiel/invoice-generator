91aa89098fcfdc08e3ab20c51d10a0357604fd13
Kipras <kipras@anaweb.lt>
2021-02-21 20:49:30 +0200

    Invoice listener increments Invoice number.

--- app/Listeners/IncrementInvoiceNumber.php
+++ app/Listeners/IncrementInvoiceNumber.php
@@ -24,6 +24,12 @@ class IncrementInvoiceNumber
      */
     public function handle(InvoiceCreated $event)
     {
-
+        $settings = json_decode($event->user->getUserSettings('sfNumberSettings')->value);
+        $settings->sf_number = strval($settings->sf_number + 1);
+        $jsonData = json_encode($settings);
+        $event->user->meta()->updateOrCreate(
+            ['name' => 'sfNumberSettings'],
+            ['value' => $jsonData]
+        );
     }
 }
