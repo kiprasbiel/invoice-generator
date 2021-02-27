aa9c7a56bf25257e38e566028dfb76761d40b12f
Kipras <kipras@anaweb.lt>
2021-02-27 19:24:51 +0200

    Created Expense factory.

---
+++ database/factories/ExpenseFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Expense;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ExpenseFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Expense::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition() {
+        return [
+            'seller_name' => $this->faker->name,
+            'seller_code' => $this->faker->randomNumber(8),
+            'seller_address' => $this->faker->address,
+            'seller_vat' => 'LT' . $this->faker->randomNumber(8),
+            'seller_country' => $this->faker->country,
+            'number' => 'SUB' . $this->faker->randomNumber(8),
+            'date' => $this->faker->dateTime(),
+            'currency' => $this->faker->currencyCode,
+            'price' => $this->faker->randomNumber(3)
+        ];
+    }
+}
