41af646ac461ca890dc197696882fa79e625a8d2
Kipras <kipras@anaweb.lt>
2021-02-28 12:15:46 +0200

    Test for creating new Expense without items

--- tests/Feature/ExpensesTest.php
+++ tests/Feature/ExpensesTest.php
@@ -2,9 +2,13 @@
 
 namespace Tests\Feature;
 
+use App\Http\Livewire\Expenses\Form;
+use App\Models\Expense;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Livewire\Livewire;
 use Tests\TestCase;
 
 class ExpensesTest extends TestCase
@@ -24,5 +28,23 @@ class ExpensesTest extends TestCase
         parent::tearDown();
 
         $this->user = NULL;
+    }
+
+    // Without expenses items
+    public function test_can_create_expense() {
+        $someResponse = Livewire::test(Form::class)
+            ->set('expenseNumber', 'SA125')
+            ->set('date', Carbon::now()->sub(7, 'days')->format('Y-m-d'))
+            ->set('currency', 'Eur')
+            ->set('sellerName', 'Topo Centras')
+            ->set('sellerAddress', 'Savanoriu pr.')
+            ->set('sellerCountry', 'Lietuva')
+            ->set('sellerCode', '159753789')
+            ->set('sellerVAT', 'LT159753789');
+
+        $someResponse->call('create');
+
+        $someResponse->assertHasNoErrors();
+        $this->assertTrue(Expense::whereSellerName('Topo Centras')->exists());
     }
 }
