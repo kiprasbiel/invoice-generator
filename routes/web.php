a2d8917e77b031235068a320d50a719262a26500
Kipras <kipras@anaweb.lt>
2021-02-28 11:57:45 +0200

    Added a button with a route to expenses/create

--- routes/web.php
+++ routes/web.php
@@ -14,26 +14,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Dashboard
 Route::middleware(['auth:sanctum', 'verified'])->get('/', function () {
     return view('dashboard');
 })->name('dashboard');
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/invoice/create', function () {
+// Invoices
+Route::middleware(['auth:sanctum', 'verified'])->get('/invoice/create', function() {
     return view('invoice.create');
 })->name('invoice.create');
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/invoice', function () {
+Route::middleware(['auth:sanctum', 'verified'])->get('/invoice', function() {
     return view('invoice.index');
 })->name('invoice.index');
-
-Route::middleware(['auth:sanctum', 'verified'])->get('/activity', function() {
-    return view('activity.show');
-})->name('activity.show');
 
 Route::middleware(['auth:sanctum', 'verified'])->middleware('can:edit-invoice,invoice')->get('/invoice/edit/{invoice}', function(Invoice $invoice) {
     return view('invoice.edit')->with('invoice', $invoice);
 })->name('invoice.edit');
 
+// Settings
+Route::middleware(['auth:sanctum', 'verified'])->get('/activity', function() {
+    return view('activity.show');
+})->name('activity.show');
+
+// Expenses
 Route::middleware(['auth:sanctum', 'verified'])->get('/expenses', function() {
     return view('expenses.index');
 })->name('expenses.index');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/expenses/create', function() {
+    return view('expenses.create');
+})->name('expenses.create');
