a958b5e9a3cfba0c31c422a912c5bb3383ef77cb
Kipras <kipras@anaweb.lt>
2021-03-07 19:46:38 +0200

    Removed redundant comment

--- resources/views/invoice/edit.blade.php
+++ resources/views/invoice/edit.blade.php
@@ -3,7 +3,6 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <h5>ID: {{ $invoice->id }}</h5>
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-{{--                @livewire('invoice-form')--}}
                 <livewire:invoice-form :invoice="$invoice"/>
             </div>
         </div>
