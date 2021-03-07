16bfc720ee8160eee04cb701daf6d638f38b10c0
Kipras <kipras@anaweb.lt>
2021-03-07 19:58:40 +0200

    Added delete and edit buttons to expanded expenses table

--- resources/views/livewire/expenses/row-list.blade.php
+++ resources/views/livewire/expenses/row-list.blade.php
@@ -104,14 +104,13 @@
             </table>
         </div>
         <div>
-            {{--            <div class="flex flex-row-reverse p-4 space-x-4 space-x-reverse">--}}
-            {{--                <button class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"--}}
-            {{--                        wire:click="delete">Ištrinti</button>--}}
-            {{--                <a class="bg-yellow-300 hover:bg-yellow-500 text-white font-bold py-2 px-4 rounded" type="submit"--}}
-            {{--                   href="{{route('expenses.edit', [$expense->id])}}">Redaguoti</a>--}}
-            {{--                <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"--}}
-            {{--                        wire:click="download">Atsisiųsti PDF</button>--}}
-            {{--            </div>--}}
+            <div class="flex flex-row-reverse p-4 space-x-4 space-x-reverse">
+                <button class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
+                        wire:click="delete">Ištrinti
+                </button>
+                <a class="bg-yellow-300 hover:bg-yellow-500 text-white font-bold py-2 px-4 rounded" type="submit"
+                   href="{{route('expenses.edit', [$expense->id])}}">Redaguoti</a>
+            </div>
         </div>
     </td>
 </tr>
