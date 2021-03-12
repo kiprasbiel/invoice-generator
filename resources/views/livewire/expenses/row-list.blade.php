ae8d8c5673ee7f350d8678bbefb96f4d870d14fa
Kipras <kipras@anaweb.lt>
2021-03-12 16:59:55 +0200

    row-list.blade changed column name

--- resources/views/livewire/expenses/row-list.blade.php
+++ resources/views/livewire/expenses/row-list.blade.php
@@ -25,7 +25,7 @@
                     </th>
                     <th scope="col"
                         class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                        Viso kaina
+                        Suma
                     </th>
                 </tr>
                 </thead>
