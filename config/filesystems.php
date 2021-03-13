8630d44c9fe7c5ce3755b00defa94c7e032326ba
Kipras <kipras@anaweb.lt>
2021-03-13 10:59:50 +0200

    Added a disk for storing imported invoices

--- config/filesystems.php
+++ config/filesystems.php
@@ -42,7 +42,10 @@ return [
     */
 
     'disks' => [
-
+        'invoices' => [
+            'driver' => 'local',
+            'root' => storage_path('app/invoices'),
+        ],
         'local' => [
             'driver' => 'local',
             'root' => storage_path('app'),
@@ -51,7 +54,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
         ],
 
