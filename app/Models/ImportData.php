c58134621a6093b0a178351de2d384a29bd2edd4
Kipras <kipras@anaweb.lt>
2021-03-28 18:23:08 +0300

    Created ImportData model and migration

---
+++ app/Models/ImportData.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ImportData extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'csv_filename',
+        'csv_header',
+        'csv_data',
+    ];
+
+    public function user() {
+        return $this->belongsTo('App\Models\User');
+    }
+}
---
+++ database/migrations/2021_03_28_120549_create_import_data_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImportDataTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('import_data', function(Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('csv_filename');
+            $table->boolean('csv_header')->default(0);
+            $table->longText('csv_data')->nullable();
+            $table->boolean('is_imported')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('import_data');
+    }
+}
