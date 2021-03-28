127a2032bc45aff2cc765b00e545b7fe2e475350
Kipras <kipras@anaweb.lt>
2021-03-28 18:14:05 +0300

    Client model added fillables and relationship to User model

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Client extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'registration_numb',
+        'vat_number',
+        'address',
+    ];
+
+    public function user() {
+        return $this->belongsTo('App\Models\User');
+    }
 }
