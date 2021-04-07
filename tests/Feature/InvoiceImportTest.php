e7c44a424200fdd1dd688207635a3c7b99b172b4
Kipras <kipras@anaweb.lt>
2021-04-07 19:40:38 +0300

    Test. Checking if parse-import and invoice-import components are present in activity settings

--- tests/Feature/InvoiceImportTest.php
+++ tests/Feature/InvoiceImportTest.php
@@ -3,6 +3,9 @@
 namespace Tests\Feature;
 
 use App\Http\Livewire\Settings\InvoiceImport;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
 use Livewire\Livewire;
@@ -10,6 +13,23 @@ use Tests\TestCase;
 
 class InvoiceImportTest extends TestCase
 {
+    use RefreshDatabase;
+    use WithFaker;
+
+    protected $user;
+
+    protected function setUp(): void {
+        parent::setUp();
+        $this->user = User::factory()->create();
+        $this->actingAs($this->user);
+    }
+
+    protected function tearDown(): void {
+        parent::tearDown();
+
+        $this->user = NULL;
+    }
+
     public function testCanUploadInvoiceFile() {
         $this->withoutExceptionHandling();
         Storage::fake('invoices');
@@ -24,6 +44,14 @@ class InvoiceImportTest extends TestCase
             ->get('fileName');
 
         Storage::disk('invoices')->assertExists($fileName);
+    }
+
+    function test_activity_page_contains_parse_import_component() {
+        $this->get('/activity')->assertSeeLivewire('parse-import');
+    }
+
+    function test_activity_page_contains_invoice_import_component() {
+        $this->get('/activity')->assertSeeLivewire('settings.invoice-import');
     }
 
 }
