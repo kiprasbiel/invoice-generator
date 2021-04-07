10fdde0844e4c675d12b755a0919103e7ef80dfe
Kipras <kipras@anaweb.lt>
2021-04-07 21:02:52 +0300

    Asserting if job is dispatched after CSV fields are selected in modal

--- tests/Feature/InvoiceImportTest.php
+++ tests/Feature/InvoiceImportTest.php
@@ -4,10 +4,12 @@ namespace Tests\Feature;
 
 use App\Http\Livewire\ParseImport;
 use App\Http\Livewire\Settings\InvoiceImport;
+use App\Jobs\InvoiceImportProcessor;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Bus;
 use Illuminate\Support\Facades\Storage;
 use Livewire\Livewire;
 use Tests\TestCase;
@@ -34,6 +36,19 @@ class InvoiceImportTest extends TestCase
     private function getCSVData(): string {
         return 'Test;Test2;Test3
 Test4;Test5;Test6';
+    }
+
+    private function getFields(): array {
+        return [
+            "fields" => [
+                10 => "price",
+                5 => "created_at",
+                4 => "company_name",
+                2 => "company_code",
+                1 => "company_vat",
+                0 => "company_code",
+            ]
+        ];
     }
 
     public function testCanUploadInvoiceFile() {
@@ -78,6 +93,16 @@ Test4;Test5;Test6';
             ->get('csvData');
         $this->assertCount(2, $csvData);
         $this->assertDatabaseCount('import_data', 1);
+    }
+
+    public function testJobDispatched() {
+        Bus::fake();
+
+        Livewire::test(ParseImport::class)
+            ->set('fields', $this->getFields())
+            ->call('startImport', 1);
+
+        Bus::assertDispatched(InvoiceImportProcessor::class);
     }
 
 }
