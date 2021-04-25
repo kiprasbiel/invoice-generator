0990237ac176e5e71a26f273a29effffa3d34885
Kipras <bielkakb@gmail.com>
2021-04-25 19:30:14 +0300

    Updated relationship InvoiceEmail to Invoice

--- app/Models/InvoiceEmail.php
+++ app/Models/InvoiceEmail.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class InvoiceEmail extends Model
 {
@@ -18,7 +18,7 @@ class InvoiceEmail extends Model
         'due_date' => 'datetime'
     ];
 
-    public function invoice(): HasOne {
-        return $this->hasOne('App\Models\Invoice');
+    public function invoice(): BelongsTo {
+        return $this->belongsTo('App\Models\Invoice');
     }
 }
