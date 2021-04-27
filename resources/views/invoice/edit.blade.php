5a3e460f57c61500a896e294a1afcc5ee309978e
Kipras <bielkakb@gmail.com>
2021-04-27 21:53:12 +0300

    Removed redundant ID text field for Invoices

--- resources/views/invoice/edit.blade.php
+++ resources/views/invoice/edit.blade.php
@@ -1,7 +1,6 @@
 <x-app-layout>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <h5>ID: {{ $invoice->id }}</h5>
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <livewire:invoice-form :invoice="$invoice"/>
             </div>
