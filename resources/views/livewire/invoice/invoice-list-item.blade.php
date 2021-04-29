0ad83317839cb32b4767ebb1dc122fbd2bd9f623
Kipras <bielkakb@gmail.com>
2021-04-29 21:28:55 +0300

    Invoice table now has is_payed field representation

--- resources/views/livewire/invoice/invoice-list-item.blade.php
+++ resources/views/livewire/invoice/invoice-list-item.blade.php
@@ -6,11 +6,15 @@
             </div>
         </div>
     </td>
-{{--    <td class="px-6 py-4 whitespace-nowrap">--}}
-{{--                <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">--}}
-{{--                  Neįntegruota funkcija--}}
-    {{--                </span>--}}
-    {{--    </td>--}}
+    <td class="px-6 py-4 whitespace-nowrap">
+        <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full @if($invoice->is_payed) bg-green-100 text-green-800 @else bg-red-100 text-red-800 @endif">
+            @if($invoice->is_payed)
+                Apmokėta
+            @else
+                Neapmokėta
+            @endif
+        </span>
+    </td>
     <td class="px-6 py-4 whitespace-nowrap">
         <div class="text-sm text-gray-900">{{$invoice->sf_code}}</div>
     </td>
--- resources/views/livewire/invoice/table.blade.php
+++ resources/views/livewire/invoice/table.blade.php
@@ -9,10 +9,10 @@
                             class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                             Data
                         </th>
-{{--                        <th scope="col"--}}
-{{--                            class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">--}}
-{{--                            Būsena--}}
-{{--                        </th>--}}
+                        <th scope="col"
+                            class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            Būsena
+                        </th>
                         <th scope="col"
                             class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                             Serija, numeris
